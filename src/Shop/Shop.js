ba6a4377f79aef2466f7e90241b232570520cee2
Laurent Maillet <nebounet@gmail.com>
2018-07-03 01:23:44 +0800

    filter products

--- src/Shop/Shop.js
+++ src/Shop/Shop.js
@@ -4,6 +4,14 @@ import {Products} from './Products/Products';
 import {Filters} from './Filters/Filters';
 import {capitalize} from '../utils/capitalize';
 
+const extractPrice = (price) => {
+  const prices = price.split('-');
+  return {
+    min: parseFloat(prices[0], 10),
+    max: parseFloat(prices[1], 10),
+  }
+};
+
 export class Shop extends Component {
   constructor(props) {
     super(props);
@@ -14,26 +22,41 @@ export class Shop extends Component {
     }
   }
 
+  filterProducts(products, filters) {
+    return products
+    // include products if no filter or the name contains at least one checked brand
+    // use lowercase in order to ignore cases
+      .filter(product => !filters.brand || filters.brand.length === 0 || filters.brand.find(brand => product.name.toLowerCase().includes(brand.toLowerCase())))
+      // include products if no filter or price is in range of one of selected price
+      // use extractPrice to get min and max value from filters`
+      .filter(product => !filters.price || filters.price.length === 0 || filters.price.find(price => {
+        const {min, max} = extractPrice(price);
+        return min <= product.price && max >= product.price;
+      }));
+  }
+
   componentDidMount() {
-    fetch('/api/products.json')
-      .then(data => data.json())
-      .then(products => this.setState({products}));
-    fetch('/api/filters.json')
-      .then(data => data.json())
-      .then(filters => filters.map(filter => ({
-        ...filter,
-        values: [...new Set(filter.values.map(value => capitalize(value)))], // remove duplicates by using a set
-      })))
-      .then(filters => this.setState({filters}));
+    return Promise.all([
+      fetch('/api/products.json')
+        .then(data => data.json())
+        .then(products => this.setState({products})),
+      fetch('/api/filters.json')
+        .then(data => data.json())
+        .then(filters => filters.map(filter => ({
+          ...filter,
+          values: [...new Set(filter.values.map(value => capitalize(value)))], // remove duplicates by using a set
+        })))
+        .then(filters => this.setState({filters})),
+    ]);
   }
 
   render() {
     return <div className="Shop">
       <div className="Shop-filters">
-        <Filters filters={this.state.filters} filtersChanged={(selectedFilters) => this.setState({selectedFilters})} />
+        <Filters filters={this.state.filters} filtersChanged={(selectedFilters) => this.setState({selectedFilters})}/>
       </div>
       <div className="Shop-products">
-        <Products products={this.state.products}/>
+        <Products products={this.filterProducts(this.state.products, this.state.selectedFilters)}/>
       </div>
     </div>;
   }
---
+++ src/Shop/Shop.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import products from '../../public/api/products';
+import filters from '../../public/api/filters';
+import {Shop} from './Shop';
+import {mount} from 'enzyme';
+
+describe('Shop component', () => {
+  let originalFetch;
+  const productsPromise = Promise.resolve({
+    ok: true,
+    json: () => products,
+  });
+  const filtersPromise = Promise.resolve({
+    ok: true,
+    json: () => filters,
+  });
+
+  beforeEach(() => {
+    originalFetch = window.fetch;
+    // mock fetch
+    // first call return products
+    // second call return filters
+    window.fetch = jest.fn()
+      .mockImplementationOnce(() => productsPromise)
+      .mockImplementationOnce(() => filtersPromise);
+  });
+
+  afterEach(() => {
+    window.fetch = originalFetch;
+  });
+
+  test('display all products when no filter is appied', async () => {
+    const wrapper = await mount(<Shop/>);
+    await Promise.all([productsPromise, filtersPromise]);
+    wrapper.update();
+    expect(wrapper.find('.Shop-Products-product').length).toBe(8);
+  });
+
+  test('should filter products depending on filters', async () => {
+    const wrapper = await mount(<Shop/>);
+    await Promise.all([productsPromise, filtersPromise]);
+    wrapper.update();
+    wrapper.find('.Shop-Filters-value input').at(0).simulate('click'); // click on nutriwell
+    // when clicking on nutriwell only 6 elements are expected
+    expect(wrapper.find('.Shop-Products-product').length).toBe(6);
+  })
+});
